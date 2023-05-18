d16c3d5fc90c33b15cc2c9372257378e39943b03
= <122753731+JadaWoods626@users.noreply.github.com>
2023-05-18 01:33:26 -0500

    assignment 199

---
+++ Dui/Dui/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dui
+{
+    class DUI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int age = Convert.ToInt32(yourAge);
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool noDui = true;
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string yourTickets = Console.ReadLine();
+            int tickets = Convert.ToInt32(yourTickets);
+            Console.ReadLine();
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (age >= 18 && noDui && tickets <= 3);
+
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
