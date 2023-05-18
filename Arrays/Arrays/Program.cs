7204d3fd9c65b2490e2cec3265fc83b2c4e8c326
= <122753731+JadaWoods626@users.noreply.github.com>
2023-05-18 10:37:20 -0500

    assignment 242

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    class Arrays
+    {
+        static void Main()
+        {
+            Console.WriteLine("Select an index of the Array");
+
+            List<string> intList = new List<string>();
+            intList.Add("Blue");
+            intList.Add("Yellow");
+            intList.Add("Purple");
+            intList.Add("Green");
+            intList.Add("Red");
+            Console.WriteLine(intList[]);
+            if (intList[] >= 4)
+            {
+                Console.WriteLine("Index Does not exist ");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+
+            Console.WriteLine("Select an index of the Array");
+            int[] numArray = new int[] { 1, 2, 3, 4, 5 };
+            numArray[5] = 6;
+            Console.WriteLine(numArray[5]);
+            Console.ReadLine();
+
+            Console.WriteLine("Select an index of the Array");
+            int[] Alphebet = new int[] { a, b, c, d, e };
+            Console.WriteLine(Alphebet);
+        }
+    }
+}
