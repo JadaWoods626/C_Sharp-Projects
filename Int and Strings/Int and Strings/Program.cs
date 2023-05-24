5a4b2f4dcd8ca9677eb18e93ef7996c992208386
= <122753731+JadaWoods626@users.noreply.github.com>
2023-05-23 21:03:58 -0500

    assignment 263

---
+++ Int and Strings/Int and Strings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Int_and_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another number.");
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two....");
+                int numThree = numOne / numTwo;
+                Console.WriteLine(numOne + " divided by " + numTwo + " equals " + numThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please do not divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
