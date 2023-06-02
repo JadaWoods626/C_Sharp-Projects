af144f3024b0eb48807176be453c8c2001f7ec7e
= <122753731+JadaWoods626@users.noreply.github.com>
2023-06-02 02:36:24 -0500

    assignment 314

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+  
+        public class Employee   
+        {
+            public int Id { get; set; } // public int class property Id
+            public List<string> FirstName { get; set; } 
+            public List<string> LastName { get; set; }  
+
+
+
+            public static bool operator ==(Employee id1, Employee id2) // overloading operator ==
+            {
+                return (id1.Id == id2.Id); // returns boolean result of equals to objects compared
+
+            }
+            public static bool operator !=(Employee id1, Employee id2) // overloading operator !=
+            {
+                return (id1.Id != id2.Id);  // returns boolean result of not equals to objects compared
+
+            }
+        }
+}
--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -10,6 +10,25 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee();  
+            Employee employee2 = new Employee();    
+
+            employee.FirstName = new List<string>() { "John" }; 
+            employee.LastName = new List<string>() { "Cena" }; 
+            employee.Id = 1;    //add value of 1 to employee property 
+
+
+            employee2.FirstName = new List<string>() { "Chris" }; 
+            employee2.LastName = new List<string>() { "Evans" }; 
+            employee2.Id = 2;   //add value of 2 to employee2 property
+
+
+
+
+            Console.WriteLine((employee == employee2)); // display result of if equals, could also return if not equal !=
+
+            Console.ReadLine(); 
+
         }
     }
 }
