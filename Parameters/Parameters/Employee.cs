3022b6e57471df923e7185b327a9582249312067
= <122753731+JadaWoods626@users.noreply.github.com>
2023-06-02 02:56:22 -0500

    Parameters

---
+++ Parameters/Parameters/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+        public class Employee<T>  
+        {
+
+            public List<T> things { get; set; } // Generic List named things
+
+
+        }
+}
---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();     
+            Employee<int> employee2 = new Employee<int>();    
+
+            employee.things = new List<string>() { "Daniel is " }; 
+            employee2.things = new List<int>() { 24 }; 
+
+            foreach (string thing in employee.things) // loop through "things"
+            {
+                foreach (int thing2 in employee2.things)
+
+                {
+                    Console.WriteLine(thing + thing2); 
+                }
+            }
+
+            Console.ReadLine(); 
+
+        }
+    }
+    }
+}
