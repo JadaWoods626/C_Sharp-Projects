c3d98975376c45831402e8a6888c67a42459ce46
= <122753731+JadaWoods626@users.noreply.github.com>
2023-06-02 16:59:07 -0500

    assignment update

---
+++ CodeFirstChallenge1/CodeFirstChallenge/CodeFirstChallenge/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstChallenge
+{
+    class StudentContext : DbContext
+    {
+        public StudentContext(): base()
+        {
+
+        }
+
+        public DbSet<Student> students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
