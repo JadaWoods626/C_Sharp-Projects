6404e36547a2e0decdfc7749bca9b8ff15b3387f
= <122753731+JadaWoods626@users.noreply.github.com>
2023-06-09 23:33:24 -0500

    Placing Code
    Organizing my code from each course in folders that go with the course repository

---
+++ Basic C# Projects/Dui/Dui/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dui
+{
+    class DUI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int age = Convert.ToInt32(yourAge);
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool noDui = Convert.ToBoolean(value: Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string yourTickets = Console.ReadLine();
+            int tickets = Convert.ToInt32(yourTickets);
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (age >= 16 && !noDui && tickets <= 3);
+
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
